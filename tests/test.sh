745b1361f59257f20ed59280999db53fff05fa08
Steve Marks <steve.marks@monii.com>
2015-09-09 14:30:25 +0100

    Revert to a simpler solution after Travis failed

--- tests/test.sh
+++ tests/test.sh
@@ -1,15 +1,6 @@
 #!/usr/bin/env /bin/sh
 
-SOURCE="${BASH_SOURCE[0]}"
-# While $SOURCE is a symlink, resolve it
-while [ -h "$SOURCE" ]; do
-    DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
-    SOURCE="$( readlink "$SOURCE" )"
-    # If $SOURCE was a relative symlink (so no "/" as prefix, need to resolve it relative to the symlink base directory
-    [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
-done
-DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
-cd $DIR;
+cd $(dirname $0);
 
 ACTUAL=$(php console.php signature-configured:optional-array-argument-command foo bar)
 
