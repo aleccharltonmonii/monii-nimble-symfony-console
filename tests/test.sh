4f21466df58a4d3820263da14cd3134d423fcbcc
Steve Marks <steve.marks@monii.com>
2015-09-09 14:27:59 +0100

    Don't hardcode cd to 'tests' in bash script. Make it flexible as possible
    Solution source: http://stackoverflow.com/questions/59895/can-a-bash-script-tell-what-directory-its-stored-in

--- tests/test.sh
+++ tests/test.sh
@@ -1,6 +1,15 @@
 #!/usr/bin/env /bin/sh
 
-cd tests;
+SOURCE="${BASH_SOURCE[0]}"
+# While $SOURCE is a symlink, resolve it
+while [ -h "$SOURCE" ]; do
+    DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+    SOURCE="$( readlink "$SOURCE" )"
+    # If $SOURCE was a relative symlink (so no "/" as prefix, need to resolve it relative to the symlink base directory
+    [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+cd $DIR;
 
 ACTUAL=$(php console.php signature-configured:optional-array-argument-command foo bar)
 
