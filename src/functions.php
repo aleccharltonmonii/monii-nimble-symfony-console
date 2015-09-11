319e1f8192148a1d7648a051872df7322af72d7b
Alec Charlton <alec.charlton@monii.com>
2015-09-11 10:37:16 +0100

    Added type hints to functions.php

--- src/functions.php
+++ src/functions.php
@@ -4,8 +4,14 @@ namespace Monii\Nimble\Symfony\Console;
 
 use Illuminate\Container\Container;
 use Monii\Nimble\Symfony\Console\Command\Command;
+use Symfony\Component\Console\Command\Command as SymfonyCommand;
 
-function containerize_command(Container $container, Command $command) {
+/**
+ * @param Container $container
+ * @param SymfonyCommand $command
+ * @return Command|SymfonyCommand
+ */
+function containerize_command(Container $container, SymfonyCommand $command) {
     if ($command instanceof Command) {
         $command->setContainer($container);
     }
